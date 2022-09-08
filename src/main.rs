c3c43f0b5d3dfd8368d17a10ed517ba57c555115
dablenparty <hdavenport8902@gmail.com>
2022-09-08 14:52:33 -0400

    Removed old comments

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     mc_server.run()?;
     match tokio::signal::ctrl_c().await {
         Ok(_) => {
-            // not sure if using ? here is a good idea... but hey, this is just an example
-            // Rust doesn't stop any child processes when the parent process exits, so we need to do it manually
             println!("Type 'stop' to stop the server");
         }
         Err(e) => {
