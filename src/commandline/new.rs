2dda1903c4dc5f706cb46ff52ccad773fac75dd7
dablenparty <hdavenport8902@gmail.com>
2023-04-09 16:38:05 -0400

    Began implementing "new" subcommand

--- src/commandline/new.rs
+++ src/commandline/new.rs
@@ -1,8 +1,8 @@
-use std::path::PathBuf;
+use std::{io, path::PathBuf};
 
 use clap::Args;
 
-use crate::ssw_error;
+use crate::{ssw_error, util::async_create_dir_if_not_exists};
 
 #[derive(Debug, Args)]
 pub struct NewArgs {
@@ -15,6 +15,18 @@ pub struct NewArgs {
     mc_version: Option<String>,
 }
 
-pub async fn new_main(_args: NewArgs) -> ssw_error::Result<()> {
-    unimplemented!("`ssw new` is not yet implemented.");
+pub async fn new_main(args: NewArgs) -> ssw_error::Result<()> {
+    let server_dir = args.server_dir;
+    if server_dir.exists() {
+        if server_dir.read_dir()?.next().is_some() {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "the specified directory is not empty",
+            )
+            .into());
+        }
+    } else {
+        async_create_dir_if_not_exists(&server_dir).await?;
+    }
+    todo!("Parse passed version and download server jar.");
 }
