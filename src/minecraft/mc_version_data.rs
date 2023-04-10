0defb3f08d8a514145672dd2fd9788ab51a37f82
dablenparty <hdavenport8902@gmail.com>
2023-04-10 19:32:17 -0400

    Removed get_required_java_version
    It's been replaced by the new MinecraftVersionData struct

--- src/minecraft/mc_version_data.rs
+++ src/minecraft/mc_version_data.rs
@@ -1,6 +1,10 @@
 use getset::Getters;
 use serde::Deserialize;
 
+use crate::ssw_error;
+
+use super::mc_version::MinecraftVersion;
+
 #[derive(Debug, Deserialize, Getters)]
 #[get = "pub"]
 pub struct VersionDownloadInfo {
@@ -29,4 +33,13 @@ pub struct MinecraftVersionData {
     id: String,
     downloads: VersionDownloadOptions,
     java_version: VersionJavaInfo,
+}
+
+impl MinecraftVersionData {
+    /// Tries to convert a `MinecraftVersion` into a `MinecraftVersionData` by making an API call.
+    /// Normally, this would be implemented as a `TryFrom` trait, but traits can't be async.
+    pub async fn async_try_from(mc_version: &MinecraftVersion) -> ssw_error::Result<Self> {
+        let response: Self = reqwest::get(&mc_version.url).await?.json().await?;
+        Ok(response)
+    }
 }
