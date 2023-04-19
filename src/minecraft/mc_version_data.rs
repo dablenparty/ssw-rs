eda706bb098ca5a9e09b29a86c4c25b1da3bbd7d
dablenparty <hdavenport8902@gmail.com>
2023-04-19 14:01:39 -0400

    Documented mc_version_data

--- src/minecraft/mc_version_data.rs
+++ src/minecraft/mc_version_data.rs
@@ -5,32 +5,42 @@ use crate::ssw_error;
 
 use super::mc_version::MinecraftVersion;
 
+/// The download info for a specific JAR related to a Minecraft version,
+/// such as the server or client JAR.
 #[derive(Debug, Deserialize, Getters)]
 #[get = "pub"]
 pub struct VersionDownloadInfo {
+    /// The URL to download the server JAR from.
     url: String,
 }
 
+/// The download options for a Minecraft version.
 #[derive(Debug, Deserialize, Getters)]
 #[get = "pub"]
 pub struct VersionDownloadOptions {
+    /// The download info for the server JAR.
     server: VersionDownloadInfo,
 }
 
+/// Info about the Java version required to run a Minecraft version.
 #[derive(Debug, Deserialize, Getters)]
 #[serde(rename_all = "camelCase")]
 #[get = "pub"]
 pub struct VersionJavaInfo {
+    /// The major version of Java required to run the server (e.g. 8, 17).
     major_version: u8,
 }
 
+/// The data for a Minecraft version.
 #[derive(Debug, Deserialize, Getters)]
 #[serde(rename_all = "camelCase")]
 #[get = "pub"]
 pub struct MinecraftVersionData {
     // My instinct is to uncomment this, but this isn't a library and I don't use this field anywhere.
     // id: String,
+    /// The download options for this version.
     downloads: VersionDownloadOptions,
+    /// The Java version required to run this version of Minecraft.
     java_version: VersionJavaInfo,
 }
 
