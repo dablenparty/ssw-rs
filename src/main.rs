93e833913107760156608235dfa527e0aa660edf
dablenparty <hdavenport8902@gmail.com>
2023-04-25 15:06:00 -0400

    Removed unnecessary Result in main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use log::{LevelFilter, info};
+use log::{info, LevelFilter};
 
 use crate::logging::init_logging;
 
 mod logging;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
     if let Err(e) = init_logging(LevelFilter::Info) {
         eprintln!("Failed to initialize logging: {}", e);
         eprintln!("Debug info: {:?}", e);
         std::process::exit(1);
     }
     info!("Hello, world!");
-    Ok(())
 }
