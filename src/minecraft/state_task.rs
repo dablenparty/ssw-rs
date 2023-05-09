ee4d469db39f627e55901356e69332d97762b111
dablenparty <hdavenport8902@gmail.com>
2023-05-08 23:57:28 -0400

    Created state_task.rs

---
+++ src/minecraft/state_task.rs
@@ -0,0 +1,52 @@
+use log::{debug, info};
+use tokio::{
+    select,
+    sync::{mpsc, watch},
+    task::JoinHandle,
+};
+use tokio_util::sync::CancellationToken;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum ServerState {
+    On,
+    Off,
+}
+
+/// Starts a task that holds the current server state.
+///
+/// This may seem like an over-abstraction, but it essentially allows us to
+/// treat server state changes as events and react to them in a more functional
+/// way.
+pub fn begin_state_task(
+    token: CancellationToken,
+) -> (
+    JoinHandle<()>,
+    (mpsc::Sender<ServerState>, watch::Receiver<ServerState>),
+) {
+    let (external_tx, mut internal_rx) = mpsc::channel::<ServerState>(2);
+    let (internal_tx, external_rx) = watch::channel(ServerState::Off);
+    let handle = tokio::spawn(async move {
+        loop {
+            select! {
+                _ = token.cancelled() => {
+                    debug!("Status task cancelled");
+                    break;
+                },
+                Some(new_state) = internal_rx.recv() => {
+                    let state_changed = internal_tx.send_if_modified(|old_state| {
+                        if *old_state == new_state {
+                            false
+                        } else {
+                            *old_state = new_state;
+                            true
+                        }
+                    });
+                    if state_changed {
+                        info!("Server state changed to {new_state:?}");
+                    }
+                }
+            }
+        }
+    });
+    (handle, (external_tx, external_rx))
+}
