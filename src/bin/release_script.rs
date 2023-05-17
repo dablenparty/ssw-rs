8c4e436d54ba76a1a1c16a0a6e2588cdeaf710d9
dablenparty <hdavenport8902@gmail.com>
2023-05-17 00:12:49 -0400

    release_script includes version number

--- src/bin/release_script.rs
+++ src/bin/release_script.rs
@@ -2,6 +2,7 @@ use std::{env, fs, io, process::Command};
 
 fn main() -> io::Result<()> {
     const PACKAGE_NAME: &str = env!("CARGO_PKG_NAME");
+    const PACKAGE_VERSION: &str = env!("CARGO_PKG_VERSION");
     const CARGO_BUILD_ARGS: &[&str] = &["build", "--release", "--bin", PACKAGE_NAME];
     let build_status = Command::new("cargo").args(CARGO_BUILD_ARGS).status()?;
     if !build_status.success() {
@@ -9,9 +10,11 @@ fn main() -> io::Result<()> {
     }
     println!("cargo build exited with status: {}", build_status);
     let old_target = format!("target/release/{}{}", PACKAGE_NAME, env::consts::EXE_SUFFIX);
+    let dotless_version = PACKAGE_VERSION.replace(".", "_");
     let new_target = format!(
-        "target/release/{}_{}_{}{}",
+        "target/release/{}_v{}_{}_{}{}",
         PACKAGE_NAME,
+        dotless_version,
         env::consts::OS,
         env::consts::ARCH,
         env::consts::EXE_SUFFIX
