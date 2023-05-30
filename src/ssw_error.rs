0dcd604b0afde75f81ce46ff03fb7742576a9d6f
dablenparty <hdavenport8902@gmail.com>
2023-05-30 16:34:20 -0400

    Removed unused file

--- src/ssw_error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum SswError {
-    #[error("SswConfigError: {0}")]
-    SswConfigError(#[from] crate::config::SswConfigError),
-    #[error("IoError: {0}")]
-    IoError(#[from] std::io::Error),
-}
